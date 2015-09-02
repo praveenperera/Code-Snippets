612310b1b4d10a39bce0e9f888be8898771c8b60
Praveen <ppraveen25@gmail.com>
2015-09-02 17:21:31 -0400

    code snippet for writing to and appending to files

---
+++ Ruby/writing_appending_to_files.rb
@@ -0,0 +1,14 @@
+##
+# Writing to file
+#
+File.open("act15.json","w") do |f|
+  f.write(JSON.pretty_generate(array))
+end
+
+
+##
+# Appending to File
+#
+File.open('act15.json', 'a') { |f|
+  f.puts(JSON.pretty_generate(array))
+end
