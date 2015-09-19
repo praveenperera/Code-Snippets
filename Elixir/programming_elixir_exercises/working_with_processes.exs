8cedd7ec4ca21d16e9e959925cc7e54beba9efc2
Praveen <ppraveen25@gmail.com>
2015-09-19 05:40:56 -0400

    changes

---
+++ Elixir/programming_elixir_exercises/working_with_processes.exs
@@ -0,0 +1,18 @@
+defmodule MyProcess do
+
+	def two_processes do
+		me = self
+		spawn(fn -> (send me, "fred") end)
+		spawn(fn -> (send me, "betty") end)
+
+		receive do
+			person -> "Token is #{person}"
+		end
+	end
+
+	def run do
+		IO.inspect two_processes
+		IO.inspect two_processes
+	end
+
+end
