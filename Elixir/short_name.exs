8033b03bcb0d44ad69b73b42e5279d0a5c054c63
Praveen <ppraveen25@gmail.com>
2015-09-24 00:02:50 -0400

    improvements

--- Elixir/short_name.exs
+++ Elixir/short_name.exs
@@ -1,19 +1,19 @@
 defmodule Shortname do
 
-	def _short_name([head | [] ], acc), do: Enum.reverse([head|acc]) |> Enum.join(" ")
-
-	def _short_name([head | tail], acc), do: _short_name(tail,[String.first(head) <>"." | acc])
-
-	def _short_name([head | tail]), do: _short_name(tail, [head])
-
 	def short_name(name) do
 		name = String.split(name, " ")
 
 		case name do
-			[_head |[]]  -> name
+			[_head |[]]  -> hd(name)
 			[_head | _tail] -> _short_name(name)
 		end
 	end
+
+	defp _short_name([head | [] ], acc), do: Enum.reverse([head|acc]) |> Enum.join(" ")
+
+	defp _short_name([head | tail], acc), do: _short_name(tail,[String.first(head) <>"." | acc])
+
+	defp _short_name([head | tail]), do: _short_name(tail, [head])
 
 
 end
