f56a7e6d9400ed067945aec6374d0d769f4eef38
Praveen <ppraveen25@gmail.com>
2015-09-24 00:26:52 -0400

    changed tabs to spaces

--- Elixir/short_name.exs
+++ Elixir/short_name.exs
@@ -1,19 +1,19 @@
 defmodule Shortname do
 
-	def short_name(name) do
-		name = String.split(name, " ")
+   def short_name(name) do
+      name = String.split(name, " ")
 
-		case name do
-			[_head |[]]  -> hd(name)
-			[_head | _tail] -> _short_name(name)
-		end
-	end
+      case name do
+         [_head |[]]  -> hd(name)
+         [_head | _tail] -> _short_name(name)
+      end
+   end
 
-	defp _short_name([head | [] ], acc), do: Enum.reverse([head|acc]) |> Enum.join(" ")
+   defp _short_name([head | [] ], acc), do: Enum.reverse([head|acc]) |> Enum.join(" ")
 
-	defp _short_name([head | tail], acc), do: _short_name(tail,[String.first(head) <>"." | acc])
+   defp _short_name([head | tail], acc), do: _short_name(tail,[String.first(head) <>"." | acc])
 
-	defp _short_name([head | tail]), do: _short_name(tail, [head])
+   defp _short_name([head | tail]), do: _short_name(tail, [head])
 
 
 end
