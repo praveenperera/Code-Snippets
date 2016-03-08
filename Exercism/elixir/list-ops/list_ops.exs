c62389aab704f1f02aeaf7f564eceaca362c5929
Praveen <ppraveen25@gmail.com>
2016-03-08 16:01:36 -0500

    fixed formatting on list_ops

--- Exercism/elixir/list-ops/list_ops.exs
+++ Exercism/elixir/list-ops/list_ops.exs
@@ -7,72 +7,40 @@ defmodule ListOps do
   # automatically imported) and so shouldn't be used either.
 
   @spec count(list) :: non_neg_integer
-  def count(l) do
-    _count(l, 0)
-  end
+  def count(l), do: _count(l, 0)
 
   @spec reverse(list) :: list
-  def reverse(l) do
-    _reverse(l, [])
-  end
+  def reverse(l), do:  _reverse(l, [])
 
   @spec map(list, (any -> any)) :: list
-  def map(l, f) do
-    _map(l,f)
-  end
+  def map(l, f), do:  _map(l,f)
 
   @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(l, f) do
-    _filter(l, f)
-  end
+  def filter(l, f), do:  _filter(l, f)
 
   @type acc :: any
   @spec reduce(list, acc, ((any, acc) -> acc)) :: acc
-  def reduce(l, acc, f) do
-    _reduce(l,acc,f)
-  end
+  def reduce(l, acc, f), do:  _reduce(l,acc,f)
 
   @spec append(list, list) :: list
-  def append(a, b) do
-    _append(a,b)
-  end
+  def append(a, b), do:  _append(a,b)
 
   @spec concat([[any]]) :: [any]
-  def concat(ll) do
-    ll |> filter(fn x -> x != [] end) |> _concat()
-  end
+  def concat(ll), do: ll |> filter(fn x -> x != [] end) |> _concat()
 
-  defp _count([_|tail], acc) do
-    _count(tail, acc + 1)
-  end
+  defp _count([], acc), do: acc
+  defp _count([_|tail], acc), do: _count(tail, acc + 1)
 
-  defp _count([], acc) do
-    acc
-  end
 
-  defp _reverse([hd |tail], reversed_list) do
-    _reverse(tail, [hd | reversed_list])
-  end
-
-  defp _reverse([], reversed_list) do
-    reversed_list
-  end
+  defp _reverse([hd |tail], reversed_list), do: _reverse(tail, [hd | reversed_list])
+  defp _reverse([], reversed_list), do: reversed_list
 
   defp _map(incoming_list, f, reversed_list \\ [])
-
-  defp _map([], _, reversed_list) do
-    reversed_list |> reverse
-  end
-
-  defp _map([hd |tail], f, reversed_list) do
-    _map(tail, f, [f.(hd)| reversed_list])
-  end
+  defp _map([], _, reversed_list), do:  reversed_list |> reverse
+  defp _map([hd |tail], f, reversed_list), do:  _map(tail, f, [f.(hd)| reversed_list])
 
   defp _filter(incoming_list, f, reversed_list \\ [])
-
-  defp _filter([], _, reversed_list) do
-    reversed_list |> reverse
-  end
+  defp _filter([], _, reversed_list), do: reversed_list |> reverse
 
   defp _filter([hd |tail], f, reversed_list) do
     case f.(hd) do
@@ -81,43 +49,18 @@ defmodule ListOps do
     end
   end
 
-  defp _reduce([], acc, _ ) do
-    acc
-  end
-
-  defp _reduce([hd |tail], acc, f) do
-    _reduce(tail, f.(hd, 0) + acc, f)
-  end
+  defp _reduce([], acc, _ ), do: acc
+  defp _reduce([hd |tail], acc, f), do: _reduce(tail, f.(hd, 0) + acc, f)
 
   defp _append(_, _, reversed_list \\  [] )
-
-  defp _append([], [], reversed_list) do
-    reversed_list |> reverse
-  end
-
-  defp _append([hd | tail], l2, reversed_list) do
-    _append(tail, l2, [hd | reversed_list])
-  end
-
-  defp _append(l1 = [], [hd | tail], reversed_list) do
-    _append(l1, tail, [hd | reversed_list])
-  end
+  defp _append([], [], reversed_list), do:  reversed_list |> reverse
+  defp _append([hd | tail], l2, reversed_list), do: _append(tail, l2, [hd | reversed_list])
+  defp _append(l1 = [], [hd | tail], reversed_list), do: _append(l1, tail, [hd | reversed_list])
 
   defp _concat(l1, acc \\ [])
+  defp _concat([], acc), do:  acc |> reverse
+  defp _concat([ [] | [tail_hd | tail_tail ] ], acc), do: _concat(tail_tail, [tail_hd | acc])
+  defp _concat([ [hd_head | []]| tail], acc), do: _concat(tail, [hd_head | acc])
+  defp _concat([ [hd_head | hd_tail] | tail], acc), do: _concat([hd_tail | tail], [hd_head | acc])
 
-  defp _concat([], acc) do
-    acc |> reverse
-  end
-
-  defp _concat([ [hd_head | []]| tail], acc) do
-    _concat(tail, [hd_head | acc])
-  end
-
-  defp _concat([ [] | [tail_hd | tail_tail ] ], acc) do
-    _concat(tail_tail, [tail_hd | acc])
-  end
-
-  defp _concat([ [hd_head | hd_tail] | tail], acc) do
-    _concat([hd_tail | tail], [hd_head | acc])
-  end
 end
