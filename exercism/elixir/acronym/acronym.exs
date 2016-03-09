c865788fedd9b88630de6082f290f6d22aacf9c5
Praveen <ppraveen25@gmail.com>
2016-03-08 19:29:11 -0500

    cleaned up acronym

--- exercism/elixir/acronym/acronym.exs
+++ exercism/elixir/acronym/acronym.exs
@@ -6,7 +6,6 @@ defmodule Acronym do
   @spec abbreviate(string) :: String.t()
   def abbreviate(string) do
     string
-     |> sanitize
      |> strip_punctuation
      |> remove_extra_spaces
      |> split_camelcase_words
@@ -15,15 +14,13 @@ defmodule Acronym do
      |> String.upcase
   end
 
-  def sanitize(string), do: string |> String.strip |> strip_punctuation
-
   def strip_punctuation(string), do: String.replace(string, ~r{[^a-zA-Z ]+}, "")
 
   def remove_extra_spaces(string), do: String.replace(string, ~r{\s\s+}, " ")
 
-  def split_on_spaces(string), do: String.split(string, " ")
-
   def split_camelcase_words(string), do: Regex.replace(~r/([[:lower:]])([[:upper:]])/, string, "\\1 \\2")
+
+  def split_on_spaces(string), do:
 
   def select_first_letter(list), do: Enum.reduce(list, "", fn x, acc -> acc <> String.first(x) end)
 end
