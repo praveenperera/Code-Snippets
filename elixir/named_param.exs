634ce173ea13bc8972d399b12a62c9a15fc5d590
Praveen <ppraveen25@gmail.com>
2016-05-10 21:12:55 -0400

    added 3 ways of doing named params in elixir

---
+++ elixir/named_param.exs
@@ -0,0 +1,33 @@
+defmodule NamedParam do
+
+  def keyword_list_params(name: name, birthday: bday) do
+      IO.puts("-------------------")
+      IO.puts name
+      IO.puts bday
+  end
+
+  def named_params(options) do
+    %{name: name, birthday: bday} = Enum.into(options, %{})
+    IO.puts("-------------------")
+    IO.puts name
+    IO.puts bday
+  end
+
+  def named_params_with_defaults(options) do
+    defaults = [name: "Praveen", birthday: "1992-03-22"]
+    %{name: name, birthday: bday} = defaults |> Keyword.merge(options) |> Enum.into(%{})
+    IO.puts("-------------------")
+    IO.puts name
+    IO.puts bday
+  end
+
+end
+
+NamedParam.keyword_list_params(name: "Praveen", birthday: "1992-03-22") #works
+NamedParam.named_params(birthday: "1992-03-22", name: "Praveen") #works
+NamedParam.named_params_with_defaults(name: "Praveen") #works
+NamedParam.named_params_with_defaults(birthday: "1992-03-22") #works
+
+# NamedParam.named_params(name: "Praveen", birthday: "1992-03-22") #works
+# NamedParam.keyword_list_params(birthday: "1992-03-22", name: "Praveen") #fails
+
