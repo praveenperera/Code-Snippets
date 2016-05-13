7d921e9d8f942a17b2a74a5f90a016fc7d83491f
Praveen <ppraveen25@gmail.com>
2016-05-13 04:06:15 -0400

    changed bday names and dates

--- elixir/named_param.exs
+++ elixir/named_param.exs
@@ -14,8 +14,16 @@ defmodule NamedParam do
   end
 
   def named_params_with_defaults(options) do
-    defaults = [name: "Praveen", birthday: "1992-03-22"]
+    defaults = [name: "Sarah", birthday: "1985-12-15"]
     %{name: name, birthday: bday} = defaults |> Keyword.merge(options) |> Enum.into(%{})
+    IO.puts("-------------------")
+    IO.puts name
+    IO.puts bday
+  end
+
+  def named_params_with_defaults_kw_list(options) do
+    defaults = [name: "Sarah", birthday: "1985-12-15"]
+    [name: name, birthday: bday] =  Keyword.merge(defaults, options)
     IO.puts("-------------------")
     IO.puts name
     IO.puts bday
@@ -23,16 +31,22 @@ defmodule NamedParam do
 
 end
 
-NamedParam.keyword_list_params(name: "Praveen", birthday: "1992-03-22") #works
+NamedParam.keyword_list_params(name: "Sarah", birthday: "1985-12-15") #works
 
-NamedParam.named_params(name: "Praveen", birthday: "1992-03-22") #works
-NamedParam.named_params(birthday: "1992-03-22", name: "Praveen") #works
-NamedParam.named_params(name: "Praveen", birthday: "1992-03-22", extra: "nonsense") #works
+NamedParam.named_params(name: "Sarah", birthday: "1985-12-15") #works
+NamedParam.named_params(birthday: "1985-12-15", name: "Sarah") #works
+NamedParam.named_params(name: "Sarah", birthday: "1985-12-15", extra: "nonsense") #works
 
 NamedParam.named_params_with_defaults(name: "George") #works
 NamedParam.named_params_with_defaults(birthday: "1992-03-30") #works
-NamedParam.named_params_with_defaults(name: "Praveen", birthday: "1992-03-22", extra: "nonsense") #works
+NamedParam.named_params_with_defaults(name: "Sarah", birthday: "1985-12-15", extra: "nonsense") #works
+NamedParam.named_params_with_defaults(extra: "nonsense", name: "Sarah",  birthday: "1985-12-15",) #works
 
-# NamedParam.keyword_list_params(name: "Praveen", birthday: "1992-03-22", extra: "nonsense") #fails
-# NamedParam.keyword_list_params(birthday: "1992-03-22", name: "Praveen") #fails
+
+# NamedParam.named_params_with_defaults_kw_list(name: "George") #fails
+# NamedParam.named_params_with_defaults_kw_list(birthday: "1992-03-30") #fails
+# NamedParam.named_params_with_defaults_kw_list(name: "Sarah", birthday: "1985-12-15", extra: "nonsense") #fails
+
+# NamedParam.keyword_list_params(name: "Sarah", birthday: "1985-12-15", extra: "nonsense") #fails
+# NamedParam.keyword_list_params(birthday: "1992-03-22", name: "Sarah") #fails
 
