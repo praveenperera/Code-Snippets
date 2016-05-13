75d6a6c8b0d49b8baef6e0ea676e343c202f29a3
Praveen <ppraveen25@gmail.com>
2016-05-13 04:06:52 -0400

    added new function with arity 2

--- elixir/functions_learning.exs
+++ elixir/functions_learning.exs
@@ -8,18 +8,22 @@ defmodule Functions do
       x * 2
    end
 
+   def fun3(x, y) do
+      x * y
+   end
+
 end
 
 
 a_fun = &(&1 * 2)
 a_fun2 = fn x -> x * 2 end
 
+
 list = [1,2,3,4,5]
 
 
 IO.inspect list |> Enum.map(Functions.fun) ## --> [2, 4, 6, 8, 10]
 IO.inspect list |> Enum.map(&Functions.fun2/1) ## --> [2, 4, 6, 8, 10]
+IO.inspect list |> Enum.map(&Functions.fun3(&1, 3)) ## --> [3, 6, 9, 12, 15]
 IO.inspect list |> Enum.map(a_fun) ## --> [2, 4, 6, 8, 10]
 IO.inspect list |> Enum.map(a_fun2) ## --> [2, 4, 6, 8, 10]
-
-
